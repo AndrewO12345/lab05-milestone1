b29fc9193a17e776c0e78658b321e244d0a2441b
Andrew <aoolson@wisc.edu>
2021-10-21 10:49:12 -0500

    Screen 2 done

--- app/src/main/java/com/example/lab05_milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab05_milestone1/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.lab05_milestone1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickFunction(View view) {
-        Log.i("Info", "Button Pressed");
+        EditText username = findViewById(R.id.username);
+        String s = username.getText().toString();
+
+        login(s);
+    }
+
+    public void login(String S) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("Message", S);
+        startActivity(intent);
     }
 }
