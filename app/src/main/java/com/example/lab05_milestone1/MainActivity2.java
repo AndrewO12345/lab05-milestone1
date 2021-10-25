098116ae37a5822f41ea862e8b22a4932362351e
Andrew <aoolson@wisc.edu>
2021-10-25 11:06:25 -0500

    Added the set up for oncreate in main2

--- app/src/main/java/com/example/lab05_milestone1/MainActivity2.java
+++ app/src/main/java/com/example/lab05_milestone1/MainActivity2.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.content.Context;
+
+import java.util.ArrayList;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -25,6 +32,29 @@ public class MainActivity2 extends AppCompatActivity {
         String str = intent.getStringExtra("Message");
         textView.setText("Welcome " + str + "!");
 
+        Context context = getApplicationContext();
+        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("notes", Context.MODE_PRIVATE,null);
+
+        DBHelper helper = new DBHelper(sqLiteDatabase);
+        ArrayList<Note> notes = helper.readNotes(str);
+
+        ArrayList<String> displayNotes = new ArrayList<>();
+        for (Note note : notes) {
+            displayNotes.add(String.format("Title:%s\nDate%s", note.getTitle(), note.getDate()));
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, displayNotes);
+        ListView listView = (ListView) findViewById(R.id.notesListView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+                intent.putExtra("noteid", position);
+                startActivity(intent);
+            }
+        });
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
